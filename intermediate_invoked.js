4019ef7c189e1b1aa4c295c17b66f3c10c35a1e4
PANKAJ DUBEY <bt21cse197@iiitn.ac.in>
2024-01-22 02:39:51 +0530

    iife concept to maintain a secure program

---
+++ intermediate_invoked.js
@@ -0,0 +1,14 @@
+(function () {
+    var msg="a nice day";
+    function greet() {
+        var msg="good night";
+        console.log(msf+" sweet dreams");
+    }
+    greet();
+
+
+
+}
+
+)();
+console.log(msg);                    // msg variable is not available due to function scope 
