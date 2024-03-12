e601588d6f6c701a71b09e0e02758b1056b9ec9d
PANKAJ DUBEY <bt21cse197@iiitn.ac.in>
2024-03-13 00:27:04 +0530

    how call stack work

---
+++ callstack.js
@@ -0,0 +1,14 @@
+var name="pankaj"
+function Person()
+{
+    console.log("execution of person function")
+    greek(name)
+    console.log("End of execution of person function")
+    
+}
+function greek(name)
+{
+    console.log("Good morning "+name)
+}
+Person()
+console.log("End of execution of program")
